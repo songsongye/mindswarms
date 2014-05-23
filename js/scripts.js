de317a8754ddce526490d6d9167221e4a3fc565e
Corey Sizemore <coreysizemore@gmail.com>
2014-05-22 21:04:58 -0700

    added sample element items on sample.html

--- js/scripts.js
+++ js/scripts.js
@@ -12,4 +12,72 @@ $(document).ready(function() {
 	  $(this).css('display','none');
 	});
 
+
+	$( "#one" ).hover(function() {
+		$( "#hh" ).text( "Grumpy Cat #1" );
+		$( "#hsh" ).text( "Subheading, Subheading, This is a subheading!" );
+	});
+
+	$( "#two" ).hover(function() {
+		$( "#hh" ).text( "Grumpy Cat #2" );
+		$( "#hsh" ).text( "Subheading, Subheading, This is a subheading!" );
+	});
+
+	$( "#three" ).hover(function() {
+		$( "#hh" ).text( "Grumpy Cat #3" );
+		$( "#hsh" ).text( "Subheading, Subheading, This is a subheading!" );
+	});
+
+	$( "#four" ).hover(function() {
+		$( "#hh" ).text( "Grumpy Cat #4" );
+		$( "#hsh" ).text( "Subheading, Subheading, This is a subheading!" );
+	});
+
+	$( "#five" ).hover(function() {
+		$( "#hh" ).text( "Grumpy Cat #5" );
+		$( "#hsh" ).text( "Subheading, Subheading, This is a subheading!" );
+	});
+
+	$( "#six" ).hover(function() {
+		$( "#hh" ).text( "Grumpy Cat #6" );
+		$( "#hsh" ).text( "Subheading, Subheading, This is a subheading!" );
+	});
+
+	$( "#seven" ).hover(function() {
+		$( "#hh" ).text( "Grumpy Cat #7" );
+		$( "#hsh" ).text( "Subheading, Subheading, This is a subheading!" );
+	});
+
+	$( "#eight" ).hover(function() {
+		$( "#hh" ).text( "Grumpy Cat #8" );
+		$( "#hsh" ).text( "Subheading, Subheading, This is a subheading!" );
+	});
+
+	$( "#nine" ).hover(function() {
+		$( "#hh" ).text( "Grumpy Cat #9" );
+		$( "#hsh" ).text( "Subheading, Subheading, This is a subheading!" );
+	});
+
+	$( "#ten" ).hover(function() {
+		$( "#hh" ).text( "Grumpy Cat #10" );
+		$( "#hsh" ).text( "Subheading, Subheading, This is a subheading!" );
+	});
+
+	$( "#eleven" ).hover(function() {
+		$( "#hh" ).text( "Grumpy Cat #11" );
+		$( "#hsh" ).text( "Subheading, Subheading, This is a subheading!" );
+	});
+
+	$( "#twelve" ).hover(function() {
+		$( "#hh" ).text( "Grumpy Cat #12" );
+		$( "#hsh" ).text( "Subheading, Subheading, This is a subheading!" );
+	});
+
+
+
+
+
+
+
+
 });
