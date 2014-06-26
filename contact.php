f300d776c5ba8181022a0de3e2bb404e0ccbd74e
Corey Sizemore <coreysizemore@gmail.com>
2014-06-25 20:21:54 -0700

    Added Contact Page

---
+++ contact.php
@@ -0,0 +1,29 @@
+
+<?php $bodyclass = 'orange'; include 'php-templates/header.php'; ?>
+
+<!-- MAIN PAGE BODY, CONTENT -->
+<div class="main">
+	<div class="gray">
+		<div class="container">
+			<div class="row">
+				<section class="col-sm-12">
+					<article>
+						<h1>Contact Information</h1>
+					</article>
+				</section>
+			</div>
+			<div class="row">
+				<section class="col-sm-12">
+					<article>
+						<p>Contact Information</p>
+						<p>Address</br>
+							Address</p>
+						<p>Number</p>
+					</article>
+				</section>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php include 'php-templates/footer.php'; ?>
